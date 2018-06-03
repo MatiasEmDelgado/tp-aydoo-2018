5f4026474d44a92cdb3eced7c772f805a4cf8c1d
Matuchinn <matias.untref@gmail.com>
2018-06-02 22:43:24 -0300

    Agregado test y logica para reemplazar el lugar del evento en el cuerpo del mail

--- MailMerger/model/MailMerger.rb
+++ MailMerger/model/MailMerger.rb
@@ -7,5 +7,8 @@ class MailMerger
   def reemplazar_nombre_evento(template, nombre_evento)
     template.gsub(/<nombre_evento>/, nombre_evento)
   end
-
+  
+  def reemplazar_lugar_evento(template, lugar_evento)
+  	template.gsub(/<lugar_del_evento>/, lugar_evento)
+  end
 end
--- MailMerger/spec/MailMerger_spec.rb
+++ MailMerger/spec/MailMerger_spec.rb
@@ -34,16 +34,21 @@ describe 'MailMerger' do
   template = data_json['template']
   nombre_contacto = data_json['contactos'][0]['nombre']
   nombre_evento = data_json['datos']['nombre_evento']
+  lugar_evento = data_json['datos']['lugar_evento']
 
   it 'El MailMerger deberia devolverme el cuerpo del mail con el nombre reemplazado' do
     cuerpo_del_mail = mailMerger.reemplazar_nombre(template, nombre_contacto)
     expect(cuerpo_del_mail.include?'Hola juan').to be_truthy
   end
 
-  it 'El MailMerger deberia devolverme el cuerpo del mail con el nombre de evento reemplazado' do
+  it 'El MailMerger deberia devolverme el cuerpo del mail con el nombre del evento reemplazado' do
     cuerpo_del_mail = mailMerger.reemplazar_nombre_evento(template, nombre_evento)
-    puts cuerpo_del_mail
     expect(cuerpo_del_mail.include?'invitando a la cena de fin de año de la UNTREF').to be_truthy
+  end
+
+  it 'El MailMerger deberia devolverme el cuerpo del mail con el lugar del evento reemplazado' do
+    cuerpo_del_mail = mailMerger.reemplazar_lugar_evento(template, lugar_evento)
+    expect(cuerpo_del_mail.include?'en el Centro de estudios (avenida Directorio 887, Caseros)').to be_truthy
   end
 
 end
