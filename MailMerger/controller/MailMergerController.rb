3123c462298f48319a922043b0346a18eb1a5dff
Matuchinn <matias.untref@gmail.com>
2018-06-12 02:03:42 -0300

    Agrego el caso de error en el request

--- MailMerger/controller/MailMergerController.rb
+++ MailMerger/controller/MailMergerController.rb
@@ -17,15 +17,19 @@ post '/mailTest' do
 end
 
 post '/enviarMail' do
-
-  content_type :json
-  json = JSON.parse(request.body.read)
-  mail = Mail_.new(json.to_json)
+  begin
+    content_type :json
+    json = JSON.parse(request.body.read)
+    mail = Mail_.new(json.to_json)
   
-  mail.contactos.each do |contacto|
-    cuerpo = MailMerger.new.obtener_cuerpo_del_mail(json, contacto.nombre)
-    MailSender.new.enviar_mail(mail.origen, contacto.mail, mail.asunto, cuerpo)
+    mail.contactos.each do |contacto|
+      cuerpo = MailMerger.new.obtener_cuerpo_del_mail(json, contacto.nombre)
+      MailSender.new.enviar_mail(mail.origen, contacto.mail, mail.asunto, cuerpo)
+    end
+    status 200
+    body({"resultado": "ok"}.to_json)
+  rescue
+    status 500
+    {"resultado" => "entrada incorrecta"}.to_json
   end
-  status 200
-  body({"resultado": "ok"}.to_json)
 end
