fe1d21dfa1a98b8798bde3c493cbbe47bd35c3a0
Matuchinn <matias.untref@gmail.com>
2018-06-11 23:54:17 -0300

    Agrego test al MailMerger para ver si el refactor mas las etiquetas especiales funcionan con mas etiquetas

--- MailMerger/model/Template.rb
+++ MailMerger/model/Template.rb
@@ -10,11 +10,13 @@ class Template
     datos = json['datos'].to_s
     @cuerpo = json['template'].to_s
     @tags = []
-    palabras_en_el_template = json.to_s.split(' ')
+    palabras_en_el_template = json.to_s.split('>')
      
     palabras_en_el_template.each do |palabra|
       if(palabra.include? '<')
-        nombre_del_tag = palabra[1, palabra.index('>')-1]
+        #puts palabra[palabra.index('<')+1,palabra.length]
+        nombre_del_tag = palabra[palabra.index('<')+1,palabra.length]
+        #nombre_del_tag = palabra[palabra.index('<'), palabra.index('>')-1]
         @tags << TagFactory.get_tag_instance(nombre_del_tag, json.to_json)
       end  
     	#if(palabra.include? '<')
--- MailMerger/spec/MailMerger_spec.rb
+++ MailMerger/spec/MailMerger_spec.rb
@@ -17,6 +17,11 @@ describe 'MailMerger' do
   data_json_con_pais = JSON.parse(data_con_pais.to_json)
   nombre_contacto_con_pais = data_json_con_pais['contactos'][0]['nombre']
 
+  data_fraude = File.read('spec/data4.json')
+  data_fraude = JSON.parse(data_fraude.to_s)
+  data_json_fraude = JSON.parse(data_fraude.to_json)
+  nombre_contacto_con_denuncia = data_json_fraude['contactos'][0]['nombre']
+
   it 'El MailMerger deberia devolverme el cuerpo del mail para mandarselo a Juan' do
 
     cuerpo_del_mail = mailMerger.obtener_cuerpo_del_mail(data_json, nombre_contacto)
@@ -35,6 +40,17 @@ describe 'MailMerger' do
     expect(cuerpo_del_mail.include?'en Jamaica').to be_truthy
     expect(cuerpo_del_mail.include?'el día 16 de Junio').to be_truthy
     expect(cuerpo_del_mail.include?'enviando un mail a fifa@fifa.com').to be_truthy
+  end
+
+  it 'El MailMerger deberia devolverme el cuerpo del segundo mail para mandarselo a un chaval que evadio impuestos' do
+
+    cuerpo_del_mail = mailMerger.obtener_cuerpo_del_mail(data_json_fraude, nombre_contacto_con_denuncia)
+    puts cuerpo_del_mail
+    expect(cuerpo_del_mail.include?'Hola Chaval').to be_truthy
+    expect(cuerpo_del_mail.include?'invitando a pagar').to be_truthy
+    expect(cuerpo_del_mail.include?'en Argentina').to be_truthy
+    expect(cuerpo_del_mail.include?'el día 5 de Diciembre').to be_truthy
+    
   end
 
 end
