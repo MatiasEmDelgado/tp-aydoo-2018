71233439ef9b6b0569f4455c2c0c7802c71f6502
Matuchinn <matias.untref@gmail.com>
2018-06-12 15:35:37 -0300

    Cambio en el modelo, el Mail tiene cuerpo y el Mailsender solo recibe un Mail como parametro

--- MailMerger/model/Mail.rb
+++ MailMerger/model/Mail.rb
@@ -3,22 +3,22 @@ require 'json'
 
 class Mail_
   
-  attr_accessor :origen, :asunto, :contactos
+  attr_accessor :origen, :asunto, :contacto, :cuerpo
 
-  def initialize(json)
-    json = JSON.parse(json)
-    @origen = json['datos']['remitente']
-    @asunto = json['datos']['asunto']
-    @contactos = []
+  def initialize(origen, asunto, contacto, cuerpo)
+    @origen = origen
+    @asunto = asunto
+    @contacto = contacto
+    @cuerpo = cuerpo
     
-    contactos_en_el_json = json['contactos']
+    #contactos_en_el_json = json['contactos']
     
-    contactos_en_el_json.each do |contacto|
-      nombre_contacto = JSON.parse(contacto.to_json)['nombre']
-      apellido_contacto = JSON.parse(contacto.to_json)['apellido']
-      mail = JSON.parse(contacto.to_json)['mail']
-      @contactos << Contacto.new(nombre_contacto.capitalize, apellido_contacto.capitalize, mail)
-    end
+    #contactos_en_el_json.each do |contacto|
+     # nombre_contacto = JSON.parse(contacto.to_json)['nombre']
+      #apellido_contacto = JSON.parse(contacto.to_json)['apellido']
+      #mail = JSON.parse(contacto.to_json)['mail']
+      #@contactos << Contacto.new(nombre_contacto.capitalize, apellido_contacto.capitalize, mail)
+    #end
   end
 
 end
