5040bb1c1bb488d5c37faf971228213a2e50c5c6
Rodrigo <rodrigo.borda.92@gmail.com>
2018-06-24 18:30:27 -0300

    Se agregan excepciones para contactos y remitente con formato invalido

--- MailMerger/model/Contacto.rb
+++ MailMerger/model/Contacto.rb
@@ -3,9 +3,21 @@ class Contacto
   attr_accessor :nombre, :apellido, :mail
 
   def initialize(nombre, apellido, mail)
-    @nombre = nombre
-    @apellido = apellido
-    @mail = mail
+  	if(mail.include? '@')
+	    @nombre = nombre
+	    @apellido = apellido
+	    @mail = mail
+	else
+      error = ContactoException.new('El mail del Contacto ' + nombre + ' ' + apellido + ' tiene un formato incorrecto')
+      puts error.message
+      raise error
+    end
   end
   
+end
+
+class ContactoException < StandardError
+  def initialize(msg)
+    super(msg)
+  end
 end
--- MailMerger/model/Mail.rb
+++ MailMerger/model/Mail.rb
@@ -6,10 +6,22 @@ class Mail_
   attr_accessor :origen, :asunto, :contacto, :cuerpo
 
   def initialize(origen, asunto, contacto, cuerpo)
-    @origen = origen
-    @asunto = asunto
-    @contacto = contacto
-    @cuerpo = cuerpo
+  	if(origen.include? '@')
+	    @origen = origen
+	    @asunto = asunto
+	    @contacto = contacto
+	    @cuerpo = cuerpo
+    else
+      error = MailException.new('El mail del remitente tiene un formato incorrecto')
+      puts error.message
+      raise error
+    end
   end
 
+end
+
+class MailException < StandardError
+  def initialize(msg)
+    super(msg)
+  end
 end
