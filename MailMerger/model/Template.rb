9d21c157ac6ba064f044186de639e8c1b8327863
Rodrigo <rodrigo.borda.92@gmail.com>
2018-06-24 18:59:32 -0300

    Se agrega excepcion para template vacio

--- MailMerger/model/Template.rb
+++ MailMerger/model/Template.rb
@@ -7,18 +7,29 @@ class Template
 
   def initialize(json)
     json = JSON.parse(json)
-    datos = json['datos'].to_s
-    @cuerpo = json['template'].to_s
-    @tags = []
-    palabras_en_el_template = json.to_s.split('>')
-     
-    palabras_en_el_template.each do |palabra|
-      if(palabra.include? '<')
-        nombre_del_tag = palabra[palabra.index('<')+1,palabra.length]
-        @tags << TagFactory.get_tag_instance(nombre_del_tag, json.to_json)
-      end  
-    	
-    end  
+    if (!json['template'].to_s.empty?)
+      datos = json['datos'].to_s
+      @cuerpo = json['template'].to_s
+      @tags = []
+      palabras_en_el_template = json.to_s.split('>')
+       
+      palabras_en_el_template.each do |palabra|
+        if(palabra.include? '<')
+          nombre_del_tag = palabra[palabra.index('<')+1,palabra.length]
+          @tags << TagFactory.get_tag_instance(nombre_del_tag, json.to_json)
+        end
+      end 
+    else
+      error = TemplateException.new('El template del mail no puede estar vacio')
+      puts error.message
+      raise error
+    end
   end
 
+end
+
+class TemplateException < StandardError
+  def initialize(msg)
+    super(msg)
+  end
 end
